84eab6f032d3141612a4927a2724f6cd6e765164
dorjepema <33113631+dorjepema@users.noreply.github.com>
2025-04-15 13:32:17 +1000

    Update script.js
    function for hamburger icon some animation deleted

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const elementsToAnimate = document.querySelectorAll('.animate-on-scroll');
+    const hamburger = document.querySelector('.hamburger');
+    const mainNav = document.querySelector('nav');
 
-    function isInViewport(element) {
-        const rect = element.getBoundingClientRect();
-        return (
-            rect.top >= -(window.innerHeight || document.documentElement.clientHeight) &&
-            rect.left >= -(window.innerWidth || document.documentElement.clientWidth) &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-          );
-    }
-
-
-    function animateElements() {
-        elementsToAnimate.forEach(element => {
-            if (isInViewport(element) && !element.classList.contains('animated')) {
-                element.classList.add('animated');
-            } else if (!isInViewport(element) && element.classList.contains('animated')) {
-                // Delay the removal of the 'animated' class by 500 milliseconds
-                //setTimeout(() => {
-                    element.classList.remove('animated');
-                //}, 500);
-            }
-        });
-    }
-    
-
-    window.addEventListener('scroll', animateElements);
-    window.addEventListener('hashchange', function() {
-        // Add a small delay to allow for scrolling to complete
-        //setTimeout(animateElements, 200);
+    hamburger.addEventListener('click', (event) => {
+        mainNav.classList.toggle('active');
+        hamburger.classList.toggle('active');
+        event.stopPropagation(); // Prevent click on hamburger from immediately closing the menu
     });
 
-    animateElements(); // Trigger on initial load for elements already in viewport
+    document.addEventListener('touchstart', (event) => {
+        const targetElement = event.target;
+        const isNavOpen = mainNav.classList.contains('active');
+        const isHamburgerClicked = targetElement === hamburger || hamburger.contains(targetElement);
+        const isNavClicked = targetElement === mainNav || mainNav.contains(targetElement);
+
+        if (isNavOpen && !isHamburgerClicked && !isNavClicked) {
+            mainNav.classList.remove('active');
+            hamburger.classList.remove('active');
+        }
+    });
 });
